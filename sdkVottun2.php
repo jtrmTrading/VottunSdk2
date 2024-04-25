79eb12840a4090043c04de443e747bdd994ef424
jtrmTrading <162365021+jtrmTrading@users.noreply.github.com>
2024-04-25 13:09:02 -0400

    Update sdkVottun2.php

--- sdkVottun2.php
+++ sdkVottun2.php
@@ -201,11 +201,11 @@ public function listCwll() {
 //---------------------------------- AQUI SE MANDA A EJECUTAR TODA LA CLASE -----------------------------------
 
 $baseUrl = 'https://api.vottun.tech';
-$apiApp ='u82kyNXg4HX--3SaWSE1Tg4TLUG6xggkyEAha0bIgdXNIBZjyrG-34FMSdXJp_dZ'; 
-$apiKey = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyY2poVEhUQ1M1NmtvSkN6M0FkUW1RVVBMZjMiLCJ0eXBlIjoiZXJwIiwiaWQiOiIiLCJ1c2VybmFtZSI6Imp0cm0udHJhZGluZ0BnbWFpbC5jb20iLCJjaWQiOiJjNjE3OTFkMS0xMDRjLTRmODEtYjQ5OS1iN2ZiNTA0NWQxYjgiLCJza3UiOlt7InIiOjExLCJzIjo4LCJlIjowfSx7InIiOjExLCJzIjo4MDAzLCJlIjowfSx7InIiOjExLCJzIjozLCJlIjowfSx7InIiOjExLCJzIjo4MDAxLCJlIjowfSx7InIiOjExLCJzIjo4MDA1LCJlIjowfSx7InIiOjExLCJzIjo4MDAyLCJlIjowfSx7InIiOjExLCJzIjo4MDEwLCJlIjowfV0sInB1YyI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCJ9.SIOqTSRH8Mx_Xzb11QfJBWJQU7oj3W222TsmQsZOp-BQxq3D4ZwF_i6L5QkOxwg7YuZM563AepuBj49t-QLDH4I22PHPtjkdrR0LpA1asSQay4icOYJiPFUfKj6k75ifhM3WCay72RocMEs3HxHgYHxlv8iDmmQmDcJavLZ2lVPMyWUojlAIfY2BOGQ2oAZ022ppmJrV5WIRqx9Cs6HWZjcoCmqzEYIxIB3bhKNR1co-sEi3XMrUQrRx3ySUbtClyCbRxdJY1U3J8peuT9U6V-OufyVtRJMH1_9w33awpNVGIyeQ30oNSUEkRja5FchjgDXyOQVaKV4x-xYrGw21nc1CF505dcTS-cpxKk7cBtP1Nj_lDn2lB_OS-vi0-2yBuiH7grVnCGcbKGlYwGezWT5Kk2Vyjud3jPjwmwPJ_wcGNtDKdA7nzhBlgi-McjDwJLYo-6YPGvJkEa95ai1jyZHcPdiL8K1uw82ud6bHR0dGXN8eNsj5oPxw1rztv71S9uQnhUKploQefsLJ12u4uzu9KtSs_3C5PZpX9-J6wieYo1o1O_rMipTGFmhq7WtFTWZiE3bEUm49ZaVce7swpYFv-0vSPkecDRYhMTnSYBYcWmz3y7DDOvS8aTASSEbiDC_mgtPGF72B5CnpOnOtr_Bc3Tdfba38NVpTwaLAmV4';
+$apiApp =''; 
+$apiKey = '';
 $network1 = 80001;
-$erc20 = "0x40D743459Ea987a4b2BD3130bde34AD65339e522";
-$wallet = "0xf62C98822b10D2EA2fF37F9aFb35494A1aAb36D1";
+$erc20 = "";
+$wallet = "";
 $sdk = new VottunSDK($baseUrl, $apiApp, $apiKey);
 
 try {
